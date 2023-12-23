0bc364cc8a2b004ec5b664531c463a0b8aa94411
FlorianV85 <flopanofficiel@gmail.com>
2023-12-23 09:03:27 +0100

    fix: rename useEventListener to useAltOnEvent

--- src/useAltOnEvent/useAltOnEvent.ts
+++ src/useAltOnEvent/useAltOnEvent.ts
@@ -12,8 +12,24 @@ type Callback = (...args: unknown[]) => void;
  * }, [])
  *
  * useEventListener('myEventName', myCallback);
+ * @deprecated Use useAltOnEvent instead
  */
 export function useEventListener(eventName: string, callback: Callback) {
+	return useAltOnEvent(eventName, callback);
+}
+
+/**
+ * Listen alt:V client event and unregister it on unmount
+ * @param {string} eventName The event name
+ * @param {Callback} callback The event callback
+ * @example
+ * const myCallback = useCallback((arg1: string, arg2: number) => {
+ *     console.log(arg1);
+ * }, [])
+ *
+ * useAltOnEvent('myEventName', myCallback);
+ */
+export function useAltOnEvent(eventName: string, callback: Callback) {
 	useEffect(() => {
 		if ('alt' in window) {
 			alt.on(eventName, callback);
